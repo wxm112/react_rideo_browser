92b999fb0063835623b08b76874024d83de4c864
may <wxm112@me.com>
2017-10-18 17:36:52 +1100

    create input bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBG8PCNOrujvz9RcYmAya5WInAW_UYTroY';
 
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render(<App />, document.querySelector('.container'));
